dd819e6b105b30c0b36958ef2e2090bbefef8ab8
Ivan <evankirk@vk.com>
2025-08-03 16:02:33 +0500

    Create CommandStart

---
+++ src/presentation/bot/commands/start.py
@@ -0,0 +1,27 @@
+from aiogram import types
+from aiogram import Router
+from aiogram.filters.command import CommandStart
+
+from ..enums import Caption
+from ..keyboards import get_inline_support_keyboard, get_main_keyboard
+from ....data_access.shemas import AuthenticatedUserFromDB
+from ..middlewares import AuthMiddleware
+from ....business_logic.dependencies import get_auth_user_service
+from .login import router as login_router
+
+
+router = Router()
+router.include_router(login_router)
+router.message.middleware(AuthMiddleware(get_auth_user_service()))
+
+@router.message(CommandStart())
+async def start(message: types.Message, user: AuthenticatedUserFromDB | None):
+    if user:
+        await message.answer(
+            Caption.SUCCESS_AUTH, 
+            reply_markup=(get_main_keyboard())
+        )
+        support_msg = await  message.answer(Caption.SUPPORT_INFO, reply_markup=get_inline_support_keyboard())
+        await support_msg.pin()
+    else:
+        await message.answer(Caption.START_MSG)
