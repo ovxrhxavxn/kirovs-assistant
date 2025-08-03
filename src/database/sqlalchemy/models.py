0aa887ecef15ddb3cd92fc06c0db594443f506fc
Ivan <evankirk@vk.com>
2025-08-03 15:56:47 +0500

    Create AuthenticatedUser sqlalchemy model

---
+++ src/database/sqlalchemy/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .annotated_types import intpk
+from .config import Base
+
+
+class AuthenticatedUser(Base):
+
+    __tablename__ = 'authenticated_users'
+
+    id: Mapped[intpk]
+    nickname: Mapped[str] = mapped_column(unique=True)
+    domain_name: Mapped[str] = mapped_column(unique=True)
