8826dba80b26fd145d69e624fd61d93629f49dad
Ivan <evankirk@vk.com>
2025-08-03 15:48:05 +0500

    Create a module with functions providing keyboards

---
+++ src/presentation/bot/keyboards.py
@@ -0,0 +1,30 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+from .enums import (
+    MainKeyboardButton, 
+    EnterCloudKeyboardButton, 
+    ResourcePath, 
+    WriteAdminButton
+)
+
+
+def get_main_keyboard():
+    kb = ReplyKeyboardBuilder()
+    for button in MainKeyboardButton:
+        kb.button(text=button)
+    kb.adjust(3)
+    return kb.as_markup()
+
+
+def get_inline_cloud_keybord():
+    kb = InlineKeyboardBuilder()
+    kb.button(text=EnterCloudKeyboardButton.NEXTCLOUD, url=ResourcePath.CLOUD)
+    kb.adjust(1)
+    return kb.as_markup()
+
+
+def get_inline_support_keyboard():
+    kb = InlineKeyboardBuilder()
+    kb.button(text=WriteAdminButton.WRITE_ADMIN, url=ResourcePath.ADMIN_TG)
+    kb.adjust(1)
+    return kb.as_markup()
