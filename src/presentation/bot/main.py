9cbf0e90f15188c7ce7225ae2e37113a675ee42f
Ivan <evankirk@vk.com>
2025-08-05 19:52:34 +0500

    Replace logic to start func

--- src/presentation/bot/main.py
+++ src/presentation/bot/main.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from aiogram import Bot, Dispatcher
 
 from .config import bot_config
@@ -17,20 +15,16 @@ from .middlewares import AuthMiddleware
 dp = Dispatcher()
 bot = Bot(token=bot_config.bot_token)
 routers = [start_router, replies_router, utils_router]
-dp.include_routers(*routers)
-
 auth_service = get_auth_user_service()
-dp.message.outer_middleware(AuthMiddleware(auth_service))
 
 async def start():
+    dp.include_routers(*routers)
+    dp.message.outer_middleware(AuthMiddleware(auth_service))
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(
             bot, 
-            auth_user_service = get_auth_user_service(),
+            auth_user_service = auth_service,
             ldap_service = get_ldap_service(),
             weather_service = get_weather_service()
         )
-
-
-if __name__ == "__main__":
-    asyncio.run(start())
