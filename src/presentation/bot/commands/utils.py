1902f6ff7d1810c6efdca7a42cd1f7fa17e9df6b
Ivan <evankirk@vk.com>
2025-08-07 22:54:09 +0500

    Rename the flip handler

--- src/presentation/bot/commands/utils.py
+++ src/presentation/bot/commands/utils.py
@@ -9,5 +9,5 @@ from ..enums import HeadsOrTails
 router = Router()
 
 @router.message(Command("flip"))
-async def ping(msg: types.Message):
+async def play_flip(msg: types.Message):
     await msg.reply(random.choice(list(HeadsOrTails)))
